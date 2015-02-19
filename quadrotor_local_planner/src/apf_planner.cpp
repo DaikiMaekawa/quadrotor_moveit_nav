c1d58df730aa476120470055e5c8e5ccd8aaf078
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2015-02-19 23:11:56 +0900

    Fix parameters of potential field

--- quadrotor_local_planner/src/apf_planner.cpp
+++ quadrotor_local_planner/src/apf_planner.cpp
@@ -69,7 +69,7 @@ public:
                 
                 dmath::Vector3D Fs;
                 for(int i=0; i < obstacles_lc.size(); i++){
-                    Fs += get_potential_force(obstacles_lc[i], 0, 0.5, 1.0, 1.5);
+                    Fs += get_potential_force(obstacles_lc[i], 0, 1.0, 1.0, 1.5);
                 }
 
                 //dmath::Vector3D g;
