a58feb7703ed8b708363b89f527e17f5266ed703
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2015-02-22 17:34:33 +0900

    Fix an bug that distance is too far

--- quadrotor_local_planner/src/apf_planner.cpp
+++ quadrotor_local_planner/src/apf_planner.cpp
@@ -59,7 +59,7 @@ public:
                     try{
                         tf_listener_.transformPoint(base_link_, p_in, p_out);
                         dmath::Vector3D obs(p_out.point.x, p_out.point.y, p_out.point.z);
-                        if(magnitude(obs) < 500){
+                        if(magnitude(obs) < 2.0){
                             obstacles_lc.push_back(obs);
                         }
                     }catch(tf::TransformException &ex){
