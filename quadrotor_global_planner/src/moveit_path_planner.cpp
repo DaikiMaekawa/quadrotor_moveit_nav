b1a100d91afde90eb1b9139594b62353b5812d45
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2015-02-25 17:50:09 +0900

    Implement sample code of MoveIt!

--- quadrotor_global_planner/src/moveit_path_planner.cpp
+++ quadrotor_global_planner/src/moveit_path_planner.cpp
@@ -1,64 +1,25 @@
-#include <pluginlib/class_loader.h>
 #include <ros/ros.h>
+#include <moveit/move_group_interface/move_group.h>
+#include <geometry_msgs/Pose.h>
 
-#include <moveit/robot_model_loader/robot_model_loader.h>
-#include <moveit/planning_pipeline/planning_pipeline.h>
-#include <moveit/planning_interface/planning_interface.h>
-#include <moveit/kinematic_constraints/utils.h>
-#include <moveit_msgs/DisplayTrajectory.h>
-#include <moveit_msgs/PlanningScene.h>
+#include <string>
 
 int main(int argc, char *argv[]){
     ros::init(argc, argv, "moveit_path_planner");
     ros::AsyncSpinner spinner(1);
     spinner.start();
     ros::NodeHandle node_handle("~");
-
-    ros::WallDuration sleep_time(20.0);
-    sleep_time.sleep();
+    ros::WallDuration time(20.0);
+    time.sleep();
     
-    robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
-    robot_model::RobotModelPtr robot_model = robot_model_loader.getModel();
-
-    planning_scene::PlanningScenePtr planning_scene(new planning_scene::PlanningScene(robot_model));
-
-    planning_pipeline::PlanningPipelinePtr planning_pipeline(new planning_pipeline::PlanningPipeline(robot_model, node_handle, "planning_plugin", "request_adapters"));
-    sleep_time.sleep();
-
-    planning_interface::MotionPlanRequest req;
-    planning_interface::MotionPlanResponse res;
-    geometry_msgs::PoseStamped pose;
-    pose.header.frame_id = "base_link";
-    pose.pose.position.x = 0.75;
-    pose.pose.position.y = 0.0;
-    pose.pose.position.z = 0.0;
-    pose.pose.orientation.w = 1.0;
-
-    std::vector<double> tolerance_pose(3, 0.01);
-    std::vector<double> tolerance_angle(3, 0.01);
-
-    req.group_name = "base";
-    moveit_msgs::Constraints pose_goal = kinematic_constraints::constructGoalConstraints("base_link", pose, tolerance_pose, tolerance_angle);
-    req.goal_constraints.push_back(pose_goal);
-    planning_pipeline->generatePlan(planning_scene, req, res);
-
-    if(res.error_code_.val != res.error_code_.SUCCESS){
-        ROS_ERROR("Could not compute plan successfully");
-        return 0;
-    }
-
-    ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
-    moveit_msgs::DisplayTrajectory display_trajectory;
-
-    ROS_INFO("Visualizing the trajectory");
-    moveit_msgs::MotionPlanResponse response;
-    res.getMessage(response);
-
-    display_trajectory.trajectory_start = response.trajectory_start;
-    display_trajectory.trajectory.push_back(response.trajectory);
-    display_publisher.publish(display_trajectory);
-
-    sleep_time.sleep();
+    move_group_interface::MoveGroup base_group("base");
+    //base_group.setPositionTarget(0, 0, 5.0);
+    base_group.setRandomTarget();
+    moveit::planning_interface::MoveGroup::Plan plan;
+    bool ret = base_group.plan(plan);
+    if(!ret) ROS_ERROR("plan was not found");
+    
+    time.sleep();
 
     return 0;
 }
